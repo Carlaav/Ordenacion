461ac6d883c47c47d2dd463d59c162ef90fb4466
Carlaav <carla2002.av@gmail.com>
2022-03-02 13:57:43 +0100

    a

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,16 @@
-
+#apartado 1
+t=[3,2,1,5,0,2]
+for i in range (len(t)):
+    for j in range(len(t)):
+        if t[i]<t[j]:
+            ay=t[i]
+            t[i]=t[j]
+            t[j]=ay
+print(t)
+#apartado 2
+t=[3,2,1,5,0,2]
+r=[]
+for i in range(len(t)):
+    r.append(min(t))
+    t.remove(min(t))
+print(r)
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,1 +1,23 @@
+from collections import defaultdict
+class Grafo:
+    def __init__(self,vertices):
+        self.grafo = defaultdict(list) 
+        self.V = vertices 
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+    def busqueda(self,v,visited,stack):
+        visited[v] = True
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.busqueda(i,visited,stack)
 
+        stack.insert(0,v)
+
+  
+    def busquedatopologica(self):
+        visited = [False]*self.V
+        stack =[]
+        for i in range(self.V):
+            if visited[i] == False:
+                self.busqueda(i,visited,stack)
+        print (stack)
