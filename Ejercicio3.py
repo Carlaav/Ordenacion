93f2e0fb0f56950bece2a7990069bc27e41aaac1
Carlaav <carla2002.av@gmail.com>
2022-03-03 16:09:18 +0100

    a

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,1 +1,16 @@
-
+def ordenacion(arr):
+    n = len(arr)
+    corte= n//2
+    while corte > 0:
+        for i in range(corte,n):  
+            aux= arr[i]
+            j = i
+            while  j >= corte and arr[j-corte] >aux:
+                arr[j] = arr[j-corte]
+                j -= corte
+  
+            arr[j] = aux
+        corte //= 2
+arr = [ 12, 34, 54, 2, 3]
+ordenacion(arr)
+print(arr)
