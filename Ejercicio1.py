fab53415a66054be4fa576baff3fae3a619eef1e
Carlaav <carla2002.av@gmail.com>
2022-03-03 15:39:54 +0100

    a

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -14,3 +14,21 @@ for i in range(len(t)):
     r.append(min(t))
     t.remove(min(t))
 print(r)
+#apartado 3
+def ordenar(t):
+    a,b,corte=1,2,len(t)
+    while a<corte:
+        if t[a-1]<=t[a]:
+            a,b=b,b+1
+        else:
+            t[a-1],t[a]=t[a],t[a-1]
+            a-=1
+            if a==0:
+                a,b=b,b+1
+    return t
+
+t=[3,2,1,5,0,2]
+print(ordenar(t))  
+  
+
+        
